261bbc835cee8e0b3167fe940ed733d4c179bd53
ELIE <genofreedomi@naver.com>
2021-02-15 16:59:21 +0900

    PreparedStatement 추가

--- src/hsoban/dao/Dao.java
+++ src/hsoban/dao/Dao.java
@@ -2,6 +2,7 @@ package hsoban.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,6 +10,7 @@ import java.sql.Statement;
 public class Dao {
 	protected Connection con;
 	protected Statement stmt;
+	protected PreparedStatement pstmt;
 	protected ResultSet rs;
 	
 	protected String defaultUrl = "jdbc:oracle:thin:@39.117.21.102:1521:xe";
