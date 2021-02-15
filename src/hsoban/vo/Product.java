afcb9286c6400d0d9fbb44e6618885c5018a4348
Heisely <ypd00062@gmail.com>
2021-02-15 22:20:48 +0900

    Product VO

--- src/hsoban/vo/Product.java
+++ src/hsoban/vo/Product.java
@@ -5,9 +5,11 @@ CREATE TABLE PRODUCT (
 	PRODUCT_ID NUMBER,
 	COLOR VARCHAR2(50),
 	NAME VARCHAR2(100),
+	CATEGORY VARCHAR2(100),
 	PRODUCT_SIZE VARCHAR2(50),
 	PRICE NUMBER,
 	DESCRIPTION VARCHAR2(1000),
+	THUMBNAIL VARCHAR2(1000),
 	CONSTRAINT PRODUCT_PK PRIMARY KEY (PRODUCT_ID, COLOR)
 );
  */
@@ -15,6 +17,7 @@ public class Product {
 	private int product_id;
 	private String color;
 	private String name;
+	private String category;
 	private String product_size;
 	private int price;
 	private String description;
@@ -25,12 +28,13 @@ public class Product {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Product(int product_id, String color, String name, String product_size, int price, String description,
-			String thumbnail) {
+	public Product(int product_id, String color, String name, String category, String product_size, int price,
+			String description, String thumbnail) {
 		super();
 		this.product_id = product_id;
 		this.color = color;
 		this.name = name;
+		this.category = category;
 		this.product_size = product_size;
 		this.price = price;
 		this.description = description;
@@ -65,6 +69,14 @@ public class Product {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
 	}
 
 	public String getProduct_size() {
