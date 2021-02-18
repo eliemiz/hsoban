dc178d139856e22de9528afe5d0f395425201dda
Heisely <ypd00062@gmail.com>
2021-02-18 20:53:34 +0900

    Product, Stock DAO 수정

--- src/hsoban/dao/Dao_Product.java
+++ src/hsoban/dao/Dao_Product.java
@@ -3,6 +3,7 @@ package hsoban.dao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import hsoban.vo.Product;
+import hsoban.vo.Stock;
 
 public class Dao_Product extends Dao {
 	// 조회(전체, 리스트)
@@ -60,25 +61,24 @@ public class Dao_Product extends Dao {
 	}
 
 	// 조회(조건, 단일) - product_id, color
-	public ArrayList<Product> getProdList(int product_id, String color) {
-		ArrayList<Product> plist = new ArrayList<Product>();
+	public Product getProdList(int product_id, String color) {
+		Product product = null;
 		try {
 			connect();
-
-			String sql = "SELECT * FROM PRODUCT WHERE product_id = ? AND color LIKE '%'|| UPPER( ? ) ||'%'";
+			
+			// 0218. color LIKE '%'||UPPER( ? )||'%' 를 color = ? 로 수정
+			String sql = "SELECT * FROM PRODUCT WHERE product_id = ? AND color = ?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, product_id);
 			pstmt.setString(2, color);
 			
 			rs = pstmt.executeQuery();
 
-			while (rs.next()) {
-				Product prod = new Product(rs.getInt("product_id"), rs.getString("color"), rs.getString("name"),
+			if (rs.next()) { // 0218 while을 if로 수정
+				product = new Product(rs.getInt("product_id"), rs.getString("color"), rs.getString("name"),
 						rs.getString("category"), rs.getString("product_size"), rs.getInt("price"),
 						rs.getString("description"), rs.getString("thumbnail"));
-				plist.add(prod);
 			}
-
 			rs.close();
 			pstmt.close();
 			con.close();
@@ -87,13 +87,105 @@ public class Dao_Product extends Dao {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return plist;
+		return product;
 	}
+	
+	// 등록 - 0218 작성. 엑셀에는 입력항목 X로 되어있으나 혹시 몰라서 작성
+		public void insertProduct(Product ins) {
+			try {
+				connect();
+				con.setAutoCommit(false);
 
+				String sql = "INSERT INTO STOCK \r\n"
+							+ "VALUES(product_id_seq.nextval,'?','?','?','?',?,'?','?')";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, ins.getColor());
+				pstmt.setString(2, ins.getName());
+				pstmt.setString(3, ins.getCategory());
+				pstmt.setString(4, ins.getProduct_size());
+				pstmt.setInt(5, ins.getPrice());
+				pstmt.setString(6, ins.getDescription());
+				pstmt.setString(7, ins.getThumbnail());
+				pstmt.executeUpdate();
+				con.commit();
+
+				pstmt.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("DB 에러: " + e.getMessage());
+				try {
+					con.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		// 수정 - 0218 작성. 엑셀에는 입력항목 X로 되어있으나 혹시 몰라서 작성
+		public void updateProduct(Product upt) {
+			try {
+				connect();
+				con.setAutoCommit(false);
+				
+				// 상품 가격을 변경할 수도 있다고 생각해서 PRICE만 변경처리 할 수 있게 작성했습니다.
+				String sql = "UPDATE PRODUCT \r\n"
+						+ " SET PRICE = ? WHERE PRODUCT_ID = ? AND COLOR LIKE '%'||UPPER( ? )||'%'";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, upt.getPrice());
+				pstmt.setInt(2, upt.getProduct_id());
+				pstmt.setString(3, upt.getColor());
+				pstmt.executeUpdate();
+				con.commit();
+
+				pstmt.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("DB 에러: " + e.getMessage());
+				try {
+					con.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("일반 에러: " + e.getMessage());
+			}
+		}
+
+		// 삭제 - 0218 작성. 엑셀에는 입력항목 X로 되어있으나 혹시 몰라서 작성
+		public void delProduct(Product del) {
+			try {
+				connect();
+				con.setAutoCommit(false);
+
+				String sql = "DELETE FROM PRODUCT WHERE PRODUCT_ID = ?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, del.getProduct_id());
+				pstmt.executeUpdate();
+				con.commit();
+
+				pstmt.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("DB 에러: " + e.getMessage());
+				try {
+					con.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("일반 에러: " + e.getMessage());
+			}
+		}
 	public static void main(String[] args) {
 		Dao_Product dao = new Dao_Product();
-		
-		ArrayList<Product> plist = dao.getProdList();
+		dao.getProdList();
 //		ArrayList<Product> plist = dao.getProdList("");
 //		ArrayList<Product> plist = dao.getProdList(0,"");
 	}
--- src/hsoban/dao/Dao_Stock.java
+++ src/hsoban/dao/Dao_Stock.java
@@ -5,60 +5,81 @@ import java.util.ArrayList;
 import hsoban.vo.Stock;
 
 public class Dao_Stock extends Dao {
-	// 조회(전체, 리스트) - NOT USING
-
-	// 조회(조건, 단일) - product_id, color
-	public ArrayList<Stock> getStock(int product_id, String color) {
+	// 조회(전체, 리스트) - NOT USING - 0218 추가(X이나 혹시몰라 작성)
+	public ArrayList<Stock> stockList(Stock stock) {
 		ArrayList<Stock> stlist = new ArrayList<Stock>();
 		try {
 			connect();
-			String sql = "SELECT * FROM STOCK WHERE product_id = ? AND color LIKE '%'||UPPER( ? )||'%'";
+			String sql = "SELECT * FROM STOCK ORDER BY PRODUCT_ID";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Stock s = new Stock(rs.getInt(1), rs.getString(2), rs.getInt(3));
+			}
+			rs.close();
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("DB 에러: " + e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("일반 에러: " + e.getMessage());
+		}
+		return stlist;
+	}
+
+	// 조회(조건, 단일) - product_id, color
+	public Stock getStock(int product_id, String color) {
+		Stock stock = null;
+		try {
+			connect();
+			// 0218. color LIKE '%'||UPPER( ? )||'%' 를 color = ? 로 수정
+			String sql = "SELECT * FROM STOCK WHERE product_id = ? AND color = ?";
 
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, product_id);
 			pstmt.setString(2, color);
 			rs = pstmt.executeQuery();
 
-			while (rs.next()) {
-				Stock stock = new Stock(rs.getInt("product_id"), rs.getString("color"), rs.getInt("stock"));
-				stlist.add(stock);
+			if (rs.next()) { // 0218. while에서 if로 변경
+				stock = new Stock(rs.getInt("product_id"), rs.getString("color"), rs.getInt("stock"));
 			}
-
 			rs.close();
 			pstmt.close();
 			con.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("DB 에러: " + e.getMessage());
 		} catch (Exception e) {
 			e.printStackTrace();
+			System.out.println("일반 에러: " + e.getMessage());
 		}
-		return stlist;
+		return stock;
 	}
 
-	// 수정
-	public void updateStock(Stock stock) {
+	// 등록 - 0218 작성. 엑셀에는 입력항목 X로 되어있으나 혹시 몰라서 작성
+	public void insertStock(Stock ins) {
 		try {
 			connect();
 			con.setAutoCommit(false);
 
-			String sql = "UPDATE STOCK SET STOCK = ? WHERE PRODUCT_ID = ? AND COLOR LIKE '%'||UPPER( ? )||'%'";
+			String sql = "INSERT INTO STOCK VALUES(?,'?',?)";
 			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, stock.getStock());
-			pstmt.setInt(2, stock.getProduct_id());
-			pstmt.setString(3, stock.getColor());
-			pstmt.executeQuery();
+			pstmt.setInt(1, ins.getProduct_id());
+			pstmt.setString(2, ins.getColor());
+			pstmt.setInt(3, ins.getStock());
+			pstmt.executeUpdate();
 			con.commit();
 
 			pstmt.close();
 			con.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("DB 에러: " + e.getMessage());
 			try {
 				con.rollback();
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 		} catch (Exception e) {
@@ -66,10 +87,65 @@ public class Dao_Stock extends Dao {
 		}
 	}
 
+	// 수정
+	public void updateStock(Stock upt) {
+		try {
+			connect();
+			con.setAutoCommit(false);
+
+			String sql = "UPDATE STOCK SET STOCK = ? WHERE PRODUCT_ID = ? AND COLOR LIKE '%'||UPPER( ? )||'%'";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, upt.getStock());
+			pstmt.setInt(2, upt.getProduct_id());
+			pstmt.setString(3, upt.getColor());
+			pstmt.executeUpdate();
+			con.commit();
+
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("DB 에러: " + e.getMessage());
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("일반 에러: " + e.getMessage());
+		}
+	}
+
+	// 삭제 - 0218 작성. 엑셀에는 입력항목 X로 되어있으나 혹시 몰라서 작성
+	public void delStock(Stock del) {
+		try {
+			connect();
+			con.setAutoCommit(false);
+
+			String sql = "DELETE FROM STOCK WHERE PRODUCT_ID = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, del.getProduct_id());
+			pstmt.executeUpdate();
+			con.commit();
+
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("DB 에러: " + e.getMessage());
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("일반 에러: " + e.getMessage());
+		}
+	}
+
 	public static void main(String[] args) {
 		Dao_Stock dao = new Dao_Stock();
-
-		ArrayList<Stock> stlist = dao.getStock(0, "");
-		dao.updateStock(new Stock(100200, "그린(유광)", 30));
 	}
 }
